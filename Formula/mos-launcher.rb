322739675c250b32e06d9313ec9ab9d534c66b8c
Benjamin Kirchner <bk@fabrik19.de>
2023-07-06 08:51:06 +0200

    first commit

---
+++ Formula/mos-launcher.rb
@@ -0,0 +1,11 @@
+cask "mos-launcher" do
+  version "2.1.0"
+  sha256 "6487153ee8813b9101c866e5ba679045a47c16c2b9d8253d18d0c9744153235c"
+
+  url "https://download.mobilitysuite.de/launcher/mac-signed-2.1.0.zip"
+  name "mos. Launcher"
+  desc "Used for developing with the Fabrik19 Mobilitysuite"
+  homepage "https://fabrik19.de/technologie-produkte/"
+
+  app "mos-launcher.app"
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Fabrik19 Mos
+
+## How do I install these formulae?
+
+`brew install fabrik19/mos/<formula>`
+
+Or `brew tap fabrik19/mos` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
