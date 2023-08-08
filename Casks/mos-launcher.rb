0bad85af4fbcc86348cc2cbdfa4987349686ecab
Benjamin Kirchner <bk@fabrik19.de>
2023-08-08 15:47:39 +0200

    V 2.3.0

--- Casks/mos-launcher.rb
+++ Casks/mos-launcher.rb
@@ -1,8 +1,8 @@
 cask "mos-launcher" do
-  version "2.2.0"
-  sha256 "43a22485169acf23f189ef5d3a0b8983dbf64c622b245c1cc44b5ee7c7218971"
+  version "2.3.0"
+  sha256 "e58bd6c38eaa7f6c33f19dd738d60e8e48590c73fde0b853e8c2bee9053f3032"
 
-  url "https://download.mobilitysuite.de/launcher/mac-2.2.0.zip"
+  url "https://download.mobilitysuite.de/launcher/mac/2.3.0/mos-launcher.zip"
   name "mos. Launcher"
   desc "Used for developing with the Fabrik19 Mobilitysuite"
   homepage "https://fabrik19.de/technologie-produkte/"
