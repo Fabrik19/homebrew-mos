72f188ddcbda44ed1c2298c4c0c1920d4a83ed7e
Benjamin Kirchner <bk@fabrik19.de>
2024-01-02 15:07:32 +0100

    New hash

--- Casks/mos-launcher.rb
+++ Casks/mos-launcher.rb
@@ -1,6 +1,6 @@
 cask "mos-launcher" do
   version "2.4.0"
-  sha256 "886022439a9b6c05489bf711843940d0daed871b0a1aba0baa0f36eb9bfd017b"
+  sha256 "171156fddb02ed9f36d4f422d9ea5110b6b4cf42bc2a5dbc8084522d934e09db"
 
   url "https://download.mobilitysuite.de/launcher/mac/2.4.0/mos-launcher.zip"
   name "mos. Launcher"
