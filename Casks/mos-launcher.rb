f59f5dd234c34dc49f2fee1fcd079153fa6daaa8
Benjamin Kirchner <bk@fabrik19.de>
2026-09-17 10:56:01 +0200

    Version 2.9.1

--- Casks/mos-launcher.rb
+++ Casks/mos-launcher.rb
@@ -1,8 +1,8 @@
 cask "mos-launcher" do
-  version "2.9.0"
-  sha256 "c17929b2068bf0695200c83f5787495f8c675e697b0e0616cb0a18c930a89e90"
+  version "2.9.1"
+  sha256 "e11e64e0c6378d917b0a9ab68a37c4095ce7f596262c32d8b71181c24d489d23"
 
-  url "https://download.mobilitysuite.de/launcher/mac/2.9.0/mos-launcher.zip"
+  url "https://download.mobilitysuite.de/launcher/mac/2.9.1/mos-launcher.zip"
   name "mos. Launcher"
   desc "Used for mobile app development with Mobility Suite mos."
   homepage "https://mobilitysuite.de/"
